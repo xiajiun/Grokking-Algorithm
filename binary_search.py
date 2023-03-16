43e798ea3a7386b4b7e0ea01293f3d30f641d533
xiajiun <51041179+xiajiun@users.noreply.github.com>
2023-03-16 18:32:14 +0900

    Create binary_search.py

---
+++ binary_search.py
@@ -0,0 +1,22 @@
+  def search_iterative(self, list, item):
+    # low and high keep track of which part of the list you'll search in.
+    low = 0
+    high = len(list) - 1
+
+    # While you haven't narrowed it down to one element ...
+    while low <= high:
+      # ... check the middle element
+      mid = (low + high) // 2
+      guess = list[mid]
+      # Found the item.
+      if guess == item:
+        return mid
+      # The guess was too high.
+      if guess > item:
+        high = mid - 1
+      # The guess was too low.
+      else:
+        low = mid + 1
+
+    # Item doesn't exist
+    return None
